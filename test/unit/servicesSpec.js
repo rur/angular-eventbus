7abbb20e90283a228eb20ecf2e0024590dfae152
Ruaidhri Devery <info@fluid.ie>
2011-08-22 23:58:28 +0100

    added unit test for scopeWatcher Service

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -1,1 +1,41 @@
 /* jasmine specs for services go here */
+
+
+describe( "ScopeWatcher Service", function(){
+   var scope, child, childConst, wService, watcher;
+   var addedCallback;
+   var removedCallback;
+   
+   beforeEach(function(){
+       scope = angular.scope();
+       wService = scope.$service("scopeWatcher");
+   });
+   
+   it("Should call added not removed", function()
+   {   
+       child = scope.$new();
+       watcher = wService(child);
+       addedCallback = jasmine.createSpy("addded callback spy");
+       watcher.onAdded(addedCallback);
+       removedCallback = jasmine.createSpy("removed callback spy");
+       watcher.onRemoved(removedCallback);
+       scope.$eval();
+       expect(addedCallback).toHaveBeenCalled();
+       expect(removedCallback).not.toHaveBeenCalled();
+   });
+   
+   it("Should call added and removed", function(){
+       var newSc = angular.scope();
+       watcher = wService(newSc);
+       addedCallback = jasmine.createSpy("addded callback spy");
+       watcher.onAdded(addedCallback);
+       removedCallback = jasmine.createSpy("removed callback spy");
+       watcher.onRemoved(removedCallback);
+       newSc.$eval();
+       expect(addedCallback).toHaveBeenCalled();
+       expect(removedCallback).not.toHaveBeenCalled();
+       scope.$eval();
+       scope.$eval();
+       expect(removedCallback).toHaveBeenCalled();
+   });
+});
