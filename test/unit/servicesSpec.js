063a9a332a8147cc6625b16e47d707469ee6d04f
Ruaidhri Devery <info@fluid.ie>
2011-08-23 01:01:35 +0100

    Added some more unit tests ahead of refactoring the eventBus code

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -39,3 +39,51 @@ describe( "ScopeWatcher Service", function(){
        expect(removedCallback).toHaveBeenCalled();
    });
 });
+
+describe("EventBus Service", function(){
+   var scope, scope2, busService, testEvent;
+   
+   beforeEach( function(){
+      scope = angular.scope();
+      scope2 = angular.scope();
+      busService = scope.$service("eventBus");
+      testEvent = "testEvent";
+   });
+   
+   it("should add and trigger a listener within the same scope", function(){
+      var bus = busService(scope);
+      var listener = jasmine.createSpy("Simple Callback Spy");
+      bus.on(testEvent, listener);
+      expect(listener).not.toHaveBeenCalled();
+      bus.emit(testEvent);
+      expect(listener).toHaveBeenCalled();
+   });
+   
+   it("should trigger a listener in an unrelated scope", function(){
+       var bus = busService(scope);
+       var bus2 = busService(scope2);
+       var listener = jasmine.createSpy("Cross Scope Event Callback Spy");
+       bus2.on(testEvent, listener);
+       bus.emit(testEvent);
+       expect(listener).toHaveBeenCalled();
+   });
+   
+   it("should send three params to a handler across scopes", function(){
+       var bus = busService(scope);
+       var bus2 = busService(scope2);
+       var arr = ["hello","world"]
+       var listener = jasmine.createSpy("Cross Scope Event Complex Callback Spy");
+       bus2.on(testEvent, listener);
+       bus.emit(testEvent, 1, arr, "test" );
+       expect(listener).toHaveBeenCalledWith(1,arr,"test");
+   });
+   
+   it("add, trigger and then remove a listener", function(){
+      var bus = busService(scope);
+      var listener = jasmine.createSpy("Simple Callback Spy");
+      bus.on(testEvent, listener);
+      bus.remove(testEvent, listener);
+      bus.emit(testEvent);
+      expect(listener).not.toHaveBeenCalled();
+   });
+});
