ded291df48ae749acc906cccc768b08fcf7033a4
Ruaidhri Devery <info@fluid.ie>
2011-09-17 16:39:38 +0100

    Added JQuery and unhide directive
    This allows us to hide specified template expressions until Angular has a chance to get going.
    
    Signed-off-by: Ruaidhri Devery <info@fluid.ie>

--- app/js/widgets.js
+++ app/js/widgets.js
@@ -1,1 +1,9 @@
 /* http://docs.angularjs.org/#!angular.widget */
+
+angular.directive("ng:unhide",function(exp, el){
+    return function(lkEl){
+        $(lkEl).find(".angular-unhide").each(function(){
+            $(this).css("visibility","visible");
+        })
+    }
+})
